31cd68626281bbf276e5c411623d5cfa2561aa0c
shenxiu <shenxiu@shenxiu.site>
2022-11-01 21:14:49 +0800

    feat: 常量定义

---
+++ shenxiu-common/shenxiu-common-core/src/main/java/site/shenxiu/common/core/constant/HtmlConstants.java
@@ -0,0 +1,49 @@
+package site.shenxiu.common.core.constant;
+/**
+ * 常用html常量
+ *
+ * @author Shenxiu
+ * @date 2022/11/1 12:41
+ */
+public interface HtmlConstants {
+
+    /**
+     * 字符串常量：HTML 空格转义 {@code "&nbsp;" -> " "}
+     */
+    String NBSP = XmlConstants.NBSP;
+
+    /**
+     * 字符串常量：HTML And 符转义 {@code "&amp;" -> "&"}
+     */
+    String AMP = XmlConstants.AMP;
+
+    /**
+     * 字符串常量：HTML 双引号转义 {@code "&quot;" -> "\""}
+     */
+    String QUOTE = XmlConstants.QUOTE;
+
+    /**
+     * 字符串常量：HTML 单引号转义 {@code "&apos" -> "'"}
+     */
+    String APOS = XmlConstants.APOS;
+
+    /**
+     * 字符串常量：HTML 小于号转义 {@code "&lt;" -> "<"}
+     */
+    String LT = XmlConstants.LT;
+
+    /**
+     * 字符串常量：HTML 大于号转义 {@code "&gt;" -> ">"}
+     */
+    String GT = XmlConstants.GT;
+
+    /**
+     * HTML标记 正则
+     */
+    String MARK_REGEX = "(<[^<]*?>)|(<[\\s]*?/[^<]*?>)|(<[^<]*?/[\\s]*?>)";
+
+    /**
+     * SCRIPT标记 正则
+     */
+    String SCRIPT_REGEX = "<[\\s]*?script[^>]*?>.*?<[\\s]*?\\/[\\s]*?script[\\s]*?>";
+}
---
+++ shenxiu-common/shenxiu-common-core/src/main/java/site/shenxiu/common/core/xss/XssValidator.java
@@ -0,0 +1,21 @@
+package site.shenxiu.common.core.xss;
+
+import cn.hutool.core.util.ReUtil;
+import site.shenxiu.common.core.constant.HtmlConstants;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 自定义xss校验注解实现
+ * 
+ * @author Shenxiu 
+ * @date 2022/11/1 13:30
+ */
+public class XssValidator implements ConstraintValidator<Xss, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        return !ReUtil.contains(HtmlConstants.MARK_REGEX, value);
+    }
+}
