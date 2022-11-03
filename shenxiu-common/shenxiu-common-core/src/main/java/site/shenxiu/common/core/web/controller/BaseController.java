8be611b7a6c183ed4e299fd1cbdf54dde2041fe9
shenxiu <shenxiu@shenxiu.site>
2022-11-03 17:36:20 +0800

    feat：添加basecontroller

---
+++ shenxiu-common/shenxiu-common-core/src/main/java/site/shenxiu/common/core/web/controller/BaseController.java
@@ -0,0 +1,23 @@
+package site.shenxiu.common.core.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * web层通用数据处理
+ * 
+ * @author Shenxiu 
+ * @date 2022/11/3 17:27
+ */
+@Slf4j
+public class BaseController {
+
+    @Resource
+    protected HttpServletRequest request;
+
+    @Resource
+    protected HttpServletResponse response;
+}
