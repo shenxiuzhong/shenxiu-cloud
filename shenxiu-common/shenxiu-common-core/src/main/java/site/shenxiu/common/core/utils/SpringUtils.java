a24bf69860a62403ca9e472dfdfb87264e81d3fd
shenxiu <shenxiu@shenxiu.site>
2022-11-09 21:24:46 +0800

    feat: spring工具类添加

---
+++ shenxiu-common/shenxiu-common-core/src/main/java/site/shenxiu/common/core/utils/SpringUtils.java
@@ -0,0 +1,11 @@
+package site.shenxiu.common.core.utils;
+
+import cn.hutool.extra.spring.SpringUtil;
+
+/**
+ * Spring工具类
+ * @author ShenXiu 
+ * @version 2022/11/8 20:03
+ */
+public class SpringUtils extends SpringUtil {
+}
