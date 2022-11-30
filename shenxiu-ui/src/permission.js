6e91dd12d57d6e0f822fa018505ec55c41348f54
shenxiu <shenxiu1@qq.com>
2022-11-30 15:27:39 +0800

    feat ： 先屏蔽登录限制

--- shenxiu-ui/src/permission.js
+++ shenxiu-ui/src/permission.js
@@ -41,7 +41,8 @@ router.beforeEach((to, from, next) => {
     }
   } else {
     // 没有token
-    if (whiteList.indexOf(to.path) !== -1) {
+    if (true) {
+      //whiteList.indexOf(to.path) !== -1
       // 在免登录白名单，直接进入
       next()
     } else {
